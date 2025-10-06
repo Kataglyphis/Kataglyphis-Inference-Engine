dc9af830ec0710139df6437998250ee77062e1a0
Jonas Heinle <jonasheinle@googlemail.com>
2025-10-06 19:50:58 +0200

    added inference plugin

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <kataglyphis_native_inference/kataglyphis_native_inference_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  KataglyphisNativeInferencePluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("KataglyphisNativeInferencePluginCApi"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
