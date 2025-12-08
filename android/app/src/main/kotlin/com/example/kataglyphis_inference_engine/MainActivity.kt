265e8784fce494d455dc1dfb324c74a77bc9e0ec
Jonas Heinle <jonas.heinle@ipa.fraunhofer.de>
2025-12-08 16:55:58 +0100

    android

--- android/app/src/main/kotlin/com/example/kataglyphis_inference_engine/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/kataglyphis_inference_engine/MainActivity.kt
@@ -1,5 +1,17 @@
 package com.example.kataglyphis_inference_engine
 
 import io.flutter.embedding.android.FlutterActivity
+import org.freedesktop.gstreamer.GStreamer
+
+import android.os.Bundle
+import android.util.Log
 
 class MainActivity: FlutterActivity()
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    GStreamer.init(this);
+  }
+}
