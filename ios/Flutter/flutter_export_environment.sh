f160d8d3e1f11731a4e8a357c4d3fe3379509f60
Jonas Heinle <jonas.heinle@ipa.fraunhofer.de>
2025-12-16 18:24:14 +0100

    added rust impl to repo

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/jones/flutter"
+export "FLUTTER_ROOT=/home/jones/develop/flutter"
 export "FLUTTER_APPLICATION_PATH=/home/jones/GitHub/Kataglyphis-Inference-Engine"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
