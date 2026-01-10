96b1087d5fe5be4a9a5779f5d809aa3b918b5850
Jonas Heinle <jonas.heinle@ipa.fraunhofer.de>
2026-01-11 00:59:46 +0100

    fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,8 @@ android {
         applicationId = "com.example.kataglyphis_inference_engine"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // my native_code_plugin requires minSdkVersion 26.
+        minSdk = 26
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
